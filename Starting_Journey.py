381017bd6cf849116b18fd971a6e99275dbaa618
Swarup Ahire <ahireswarup2008@gmail.com>
2026-07-09 18:40:09 +0530

    First-Commit

---
+++ Starting_Journey.py
@@ -0,0 +1,8 @@
+#My journey into programming starts here.
+#Learning the ropes.
+#Learning Git and GitHub.
+#Cloning repository from GitHub to local working space.
+#Adding and committing file for the first time
+#Pushing file to GitHub again.
+
+print("Hello, World!")
